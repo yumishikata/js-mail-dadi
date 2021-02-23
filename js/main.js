9f0651592e7d81cf44f6cd1c62ea8cb3fee34e71
Yumi Shikata <yumishika17@gmail.com>
2021-02-23 14:36:48 +0100

    Push dadi

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.getElementById("input_submit").addEventListener("click" , 
+document.getElementById("email_submit").addEventListener("click" , 
 function() {
     var email_lista = ["yumishika17@gmail.com" , "paolo.dossetto@gmail.com", "paolo.mistretta@gmail.com", "filippo.bommarito@gmail.com"];
     var email = document.getElementById("get_email").value;
@@ -12,3 +12,22 @@ function() {
 }
 )
 
+
+document.getElementById("dadi_submit").addEventListener("click" , 
+function() {
+    var nome = document.getElementById("get_nome").value;
+    var nome_numero = Math.floor(Math.random() * 5 + 1);
+    var pc_numero = Math.floor(Math.random() * 5 + 1);
+    document.getElementById("dado_nome").innerHTML = nome + " " + nome_numero;
+    document.getElementById("dado_pc").innerHTML = "Computer" + " " + pc_numero;
+    if (nome_numero > pc_numero) {
+        document.getElementById("vincitore").innerHTML = nome + ", HAI VINTO";
+    }
+    else if (nome_numero == pc_numero) {
+        document.getElementById("vincitore").innerHTML = "DAI " + nome + ", E' UN PAREGGIO";
+    }
+    else {
+        document.getElementById("vincitore").innerHTML = "MI DISPIACE " + nome + ", HAI PERSO";
+    }
+}
+)
